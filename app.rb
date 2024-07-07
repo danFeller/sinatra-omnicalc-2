6ee24a0df23b0f2dea4884e312b6b6b4e3ae13f8
Daniel Feller <70229820+danFeller@users.noreply.github.com>
2024-07-07 17:20:48 +0000

    Finished

--- app.rb
+++ app.rb
@@ -5,14 +5,11 @@ get("/") do
   redirect("/add")
 end
 
-get("/ad") do
+get("/add") do
   erb(:add_form)
 end
 
 get("/wizard_add") do
-  @first_num = params.fetch("first_num").to_f
-
-  @result = @first_num + @second_num
   erb(:add_result)
 end
 
@@ -21,17 +18,14 @@ get("/subtract") do
 end
 
 get("/wizard_subtract") do
-  @first_num = params.fetch("first_num").to_f
-  @second_num = params.fetch("second_num").to_f
-
   erb(:sub_result)
 end
 
 get("/multiply") do
-  erb(:multiplication_form)
+  erb(:mult_form)
 end
 
-get("/wizard multiply") do
+get("/wizard_multiply") do
   erb(:mult_result)
 end
 
